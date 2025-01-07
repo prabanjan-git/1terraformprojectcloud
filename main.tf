5c626c8b8ae307649c8c420d833f56086f39ad55
root <root@ip-172-31-16-216.us-east-2.compute.internal>
2025-01-07 10:06:38 +0000

    1st commit tfcloud

---
+++ main.tf
@@ -0,0 +1,93 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+}
+resource "aws_vpc" "myvpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "myvpc"
+  }
+}
+resource "aws_subnet" "pubsbnt" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "pubsbnt"
+  }
+}
+resource "aws_subnet" "pvtsbnt" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-2b"
+
+  tags = {
+    Name = "pvtsbnt"
+  }
+}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
+
+  tags = {
+    Name = "igw"
+  }
+}
+resource "aws_route_table" "pubrt" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+
+  tags = {
+    Name = "pubrt"
+  }
+}
+resource "aws_route_table_association" "pubsbntpubrtassociation" {
+  subnet_id      = aws_subnet.pubsbnt.id
+  route_table_id = aws_route_table.pubrt.id
+}
+resource "aws_eip" "myeip" {
+  
+}
+resource "aws_nat_gateway" "natgw" {
+  allocation_id = aws_eip.myeip.id
+  subnet_id     = aws_subnet.pubsbnt.id
+
+  tags = {
+    Name = "gwNAT"
+  }
+
+ 
+}
+resource "aws_route_table" "pvtrt" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.natgw.id
+  }
+
+ 
+  tags = {
+    Name = "pvtrt"
+  }
+}
+resource "aws_route_table_association" "pvtsbntpvtrtassocication" {
+  subnet_id      = aws_subnet.pvtsbnt.id
+  route_table_id = aws_route_table.pvtrt.id
+}
