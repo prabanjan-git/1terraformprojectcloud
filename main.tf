21d4edeeb4b4940887456ff949b7aad91f9bb603
prabanjan <prabanjan@gmail.com>
2025-01-08 15:17:54 +0530

    3rd commit-vsc-wrongly commited

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 provider "aws" {
   region = "us-east-2"
 }
-resource "aws_vpc" "myvpc" {
+resourc "aws_vpc" "myvpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
