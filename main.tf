fb2db9a1bbaac2882dcd42b1f236de61404946e4
prabanjan <prabanjan@gmail.com>
2025-04-04 14:53:26 +0530

    7th commit bugfixed

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 provider "aws" {
   region = "ap-south-1"
 }
-resourc "aws_vpc" "myvpc" {
+resource "aws_vpc" "myvpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
