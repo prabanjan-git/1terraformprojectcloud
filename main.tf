3a698923627ea89092ac47f963ce2eae1e1e36db
prabanjan <prabanjan@gmail.com>
2025-04-04 14:48:42 +0530

    6th commit

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 provider "aws" {
   region = "ap-south-1"
 }
-resource "aws_vpc" "myvpc" {
+resourc "aws_vpc" "myvpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
